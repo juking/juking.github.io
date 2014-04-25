112c4912123679650ccd2e7dfd37c3b85bdbf7ba
juking <wjuking@gmail.com>
2014-04-25 15:37:08 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,42 @@
-console.log('This would be the main JS file.');
+$(function () {
+    $("input").bind("keydown", keySearch)
+        .attr("autocomplete", "off").css({ "position": "absolute", "left": " 90px" })
+        .each(function (i) {
+            this.style.top = 150 + (i * 40) + "px";
+            this.setAttribute("tabindex", i + 1);
+        });
+
+    function keySearch(event) {
+        if (event.keyCode == 13) {
+            if (this.value.length < 1) return false;
+            var loc;
+            switch (this.id) {
+                case "kwBD":
+                    loc = 'http://www.baidu.com/s?wd=' + (this.value);
+                    break;
+                case "kwGoogle":
+                    loc = 'https://www.google.com.hk/#newwindow=1&safe=strict&q=' + (this.value);
+                    break;
+                case "kwBing":
+                    loc = 'http://cn.bing.com/search?&qs=ED&form=QBLH&pq=a&sc=8-1&sp=1&q=' + (this.value);
+                    break;
+                case "kwYinYueTai":
+                    loc = 'http://so.yinyuetai.com/mv?keyword=' + (this.value);
+                    break;
+                case "kwBDPan":
+                    loc = 'http://www.baidu.com/s?wd=' + (this.value) + '%20site:pan.baidu.com';
+                    break;
+                default:
+                    return false;
+            }
+            window.open(loc, "newwindow");
+
+        } else {
+            var id = this.id;
+            $('input').each(function () {
+                if (id != this.id) $(this).val("");
+            });
+        }
+        return false;
+    }
+});
